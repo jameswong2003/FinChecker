5d1ebbd9e0651b183cb1a9f06ca7ce54b85aec08
James <wong.james2003@gmail.com>
2023-03-09 15:17:08 -0500

    added log out functionality

--- app.py
+++ app.py
@@ -57,6 +57,7 @@ def monthly_spending():
     if request.method == 'POST':
         form_month = request.form.get('form_month')
         entries = db.grab_month(month=form_month)
+
     return render_template('monthly-spending.html', entries=entries)
 
 
